314f47da0cd92c7c8477ff5935f4c68e45b695ef
pierre-badra <PierreBadra@outlook.com>
2025-05-25 01:09:03 -0400

    Added tailwind to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./*.php",
-    "./**/*.php",
-    "./assets/js/**/*.js",
-  ],
+  content: ["./*.php", "./**/*.php", "./assets/js/**/*.js"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Oswald', 'ui-sans-serif', 'system-ui'],
+        sans: ["Oswald", "Inter", "ui-sans-serif", "system-ui"],
       },
     },
   },
   plugins: [],
-}
+};
