0252e9f995bc63893bf5dad8dce5f930ddee0c69
pierre-badra <PierreBadra@outlook.com>
2025-05-25 01:09:28 -0400

    Completed hero section on desktop view. Need to make it responsive

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -1,3 +1,42 @@
-<section>
-    
+<section class="relative bg-gray-900 text-white overflow-hidden p-8 pt-24">
+    <!-- Background Image/Overlay -->
+    <div class="absolute inset-0">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/hero-bg.jpg" alt="Sports field background"
+            class="w-full h-full object-cover opacity-60">
+        <div class="absolute inset-0 bg-gray-900 bg-opacity-60"></div>
+    </div>
+
+    <div class="relative flex flex-col items-start justify-center max-w-[720px] min-h-[90vh] w-1/2 text-start">
+
+        <!-- Title -->
+        <h1 class="text-7xl/25 font-medium tracking-widest font-[Oswald]">
+            LES JEUNES AILÉS DE GATINEAU
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.webp"
+                alt="Les Jeunes Ailés de Gatineau Logo" class="inline w-30 h-30 drop-shadow-lg">
+        </h1>
+
+        <!-- Slogan -->
+        <p class="text-2xl font-light opacity-80 mb-11">
+            Là où renaît le talent
+        </p>
+
+        <h2 class="text-3xl font-[Oswald] tracking-widest font-medium">
+            INSCRIPTIONS POUR L’ÉTÉ 2025
+        </h2>
+        <!-- Announcement -->
+        <p class="text-2xl font-light opacity-80 mb-14">
+            Toute l’équipe de Les Jeunes Ailés de Gatineau est fière de vous annoncer que les inscriptions pour nos
+            programmes d’été 2025 sont maintenant ouvertes !
+        </p>
+
+        <!-- Call to Action Button -->
+        <a href="#"
+            class="w-full bg-[#092049] hover:bg-[#143267] border border-white text-white font-bold py-5 tracking-widest text-lg text-center transition">
+            INSCRIVEZ-VOUS MAINTENANT
+        </a>
+    </div>
 </section>
+
+<script>
+
+</script>
