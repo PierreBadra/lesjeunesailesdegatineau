8540af5701b88d269c969b5d0a67329e8849579f
pierre-badra <PierreBadra@outlook.com>
2025-05-25 01:06:32 -0400

    Removed previous code to add reusable shortcode for the hero section

--- functions.php
+++ functions.php
@@ -1,15 +1,7 @@
 <?php
-function mytheme_enqueue_styles()
-{
-  wp_enqueue_style('mytheme-style', get_stylesheet_uri(), array(), filemtime(get_stylesheet_directory() . '/style.css'));
+function my_hero_shortcode() {
+    ob_start();
+    get_template_part('template-parts/hero');
+    return ob_get_clean();
 }
-add_action('wp_enqueue_scripts', 'mytheme_enqueue_styles');
-
-// Register navigation menu
-function mytheme_register_menus()
-{
-  register_nav_menus([
-    'main-menu' => __('Main Menu', 'mytheme')
-  ]);
-}
-add_action('after_setup_theme', 'mytheme_register_menus');
+add_shortcode('hero', 'my_hero_shortcode');
