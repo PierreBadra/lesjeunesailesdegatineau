fc4fb807f25394d6ce0eb384b813688d5b3800eb
pierre-badra <PierreBadra@outlook.com>
2025-05-25 22:17:03 -0400

    Added code to remove certain sidebar items from admin panel

--- functions.php
+++ functions.php
@@ -1,7 +1,34 @@
 <?php
+
+// Shortcodes
 function my_hero_shortcode() {
     ob_start();
     get_template_part('template-parts/hero');
     return ob_get_clean();
 }
 add_shortcode('hero', 'my_hero_shortcode');
+
+
+// Actions
+add_action('admin_menu', function() {
+    // Remove Posts
+    remove_menu_page('edit.php');
+    // Remove Pages
+    // remove_menu_page('edit.php?post_type=page');
+    // Remove Comments
+    remove_menu_page('edit-comments.php');
+    // Remove Appearance
+    // remove_menu_page('themes.php');
+    // Remove Plugins
+    remove_menu_page('plugins.php');
+    // Remove Tools
+    remove_menu_page('tools.php');
+    // Remove Settings
+    // remove_menu_page('options-general.php');
+    // Remove Media
+    // remove_menu_page('upload.php');
+    // Remove Users
+    // remove_menu_page('users.php');
+    // Remove Custom Post Type (example: 'collection')
+    remove_menu_page('edit.php?post_type=collection');
+});
