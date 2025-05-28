242f535c54a40b84c1be1214ac919ecbe6d0731e
pierre-badra <PierreBadra@outlook.com>
2025-05-28 03:24:06 -0400

    Made index.php page code more readable

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
-<?php get_header() ?>
-<?php
-if (have_posts()):
-    while (have_posts()):
-        the_post();
-        the_content();
-    endwhile;
-else:
-    echo '<p>No content found.</p>';
-endif;
-?>
-<?php get_footer() ?>
+<?php get_header(); ?>
+
+<?php if (have_posts()): ?>
+    <?php while (have_posts()):
+        the_post(); ?>
+        <?php the_content(); ?>
+    <?php endwhile; ?>
+<?php else: ?>
+    <p>No content found.</p>
+<?php endif; ?>
+
+<?php get_footer(); ?>
