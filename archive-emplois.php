d334744de71b33923b32d9e226970dbbc8d62bd1
pierre-badra <PierreBadra@outlook.com>
2025-05-30 22:46:42 -0400

    Added empty state for list of emplois

--- archive-emplois.php
+++ archive-emplois.php
@@ -38,100 +38,135 @@
                     src="<?= get_template_directory_uri() ?>/assets/images/default-image.svg" alt="Image d'avant page">
             </div>
         </div>
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-16">
-            <?php
-            $collections = get_posts([
-                'post_type' => 'emplois',
-                'post_status' => 'publish',
-                'order' => 'ASC',
-            ]);
-
-            foreach ($collections as $collection):
-                $titre = get_field('titre', $collection->ID);
-                $salaire_par_heure = get_field('salaire_par_heure', $collection->ID);
-                $type_demploi = get_field('$type_demploi', $collection->ID);
-                $date_limite_pour_la_candidature = get_field('date_limite_pour_la_candidature', $collection->ID);
-                $description = get_field('description', $collection->ID);
-                ?>
-                <a href="<?= get_permalink($collection->ID); ?>" class="group">
-                    <div class="py-6">
-                        <div class="flex items-center justify-between mb-3">
-                            <div
-                                class="flex items-center gap-1 text-blue-950 bg-amber-300 justify-center px-4 py-1 rounded-md w-fit">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                    stroke-linejoin="round" class="lucide lucide-briefcase w-4 h-4">
-                                    <path d="M16 20V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path>
-                                    <rect width="20" height="14" x="2" y="6" rx="2"></rect>
-                                </svg>
-                                <div class="inline-flex items-center rounded-full border font-semibold 
+        <?php
+        $collections = get_posts([
+            'post_type' => 'emplois',
+            'post_status' => 'publish',
+            'order' => 'ASC',
+        ]); ?>
+        <h2
+            class="font-semibold flex mb-6 items-center gap-2 text-2xl bg-gradient-to-r from-slate-900 via-blue-900 to-slate-800 bg-clip-text text-transparent uppercase font-[Oswald] tracking-widest">
+            Nos postes ouverts</h2>
+        <?php if ($collections): ?>
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-16">
+                <?php foreach ($collections as $collection):
+                    $titre = get_field('titre', $collection->ID);
+                    $salaire_par_heure = get_field('salaire_par_heure', $collection->ID);
+                    $type_demploi = get_field('$type_demploi', $collection->ID);
+                    $date_limite_pour_la_candidature = get_field('date_limite_pour_la_candidature', $collection->ID);
+                    $description = get_field('description', $collection->ID);
+                    ?>
+                    <a href="<?= get_permalink($collection->ID); ?>" class="group">
+                        <div class="py-6">
+                            <div class="flex items-center justify-between mb-3">
+                                <div
+                                    class="flex items-center gap-1 text-blue-950 bg-amber-300 justify-center px-4 py-1 rounded-md w-fit">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round" class="lucide lucide-briefcase w-4 h-4">
+                                        <path d="M16 20V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path>
+                                        <rect width="20" height="14" x="2" y="6" rx="2"></rect>
+                                    </svg>
+                                    <div class="inline-flex items-center rounded-full border font-semibold 
                                     transition-colors focus:outline-none focus:ring-2 focus:ring-ring 
                                     focus:ring-offset-2 border-transparent bg-secondary text-secondary-foreground 
                                     hover:bg-secondary/80 text-xs font-[Inter]">
-                                    Emplois
+                                        Emplois
+                                    </div>
+                                </div>
+                                <div class="flex items-center gap-2 text-sm text-gray-500">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round" class="lucide lucide-calendar w-4 h-4">
+                                        <path d="M8 2v4"></path>
+                                        <path d="M16 2v4"></path>
+                                        <rect width="18" height="18" x="3" y="4" rx="2"></rect>
+                                        <path d="M3 10h18"></path>
+                                    </svg>
+                                    <?php
+                                    $date = new DateTime($collection->post_date);
+                                    $formatter = new IntlDateFormatter(
+                                        'fr_FR',
+                                        IntlDateFormatter::LONG,
+                                        IntlDateFormatter::NONE,
+                                        'America/Toronto',
+                                        IntlDateFormatter::GREGORIAN,
+                                        'd MMMM yyyy'
+                                    );
+                                    ?>
+                                    <span><?= $formatter->format($date) ?></span>
                                 </div>
                             </div>
-                            <div class="flex items-center gap-2 text-sm text-gray-500">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                    stroke-linejoin="round" class="lucide lucide-calendar w-4 h-4">
-                                    <path d="M8 2v4"></path>
-                                    <path d="M16 2v4"></path>
-                                    <rect width="18" height="18" x="3" y="4" rx="2"></rect>
-                                    <path d="M3 10h18"></path>
-                                </svg>
-                                <?php
-                                $date = new DateTime($collection->post_date);
-                                $formatter = new IntlDateFormatter(
-                                    'fr_FR',
-                                    IntlDateFormatter::LONG,
-                                    IntlDateFormatter::NONE,
-                                    'America/Toronto',
-                                    IntlDateFormatter::GREGORIAN,
-                                    'd MMMM yyyy'
-                                );
-                                ?>
-                                <span><?= $formatter->format($date) ?></span>
-                            </div>
-                        </div>
-                        <h3
-                            class="truncate w-full text-xl font-medium bg-gradient-to-r from-slate-900 via-blue-900 to-slate-800 bg-clip-text text-transparent mb-3 uppercase font-[Oswald] tracking-widest">
-                            <?= $titre ?>
-                        </h3>
-                        <?php
-                        // Get the WYSIWYG content
-                        $description = get_field('description', $collection->ID);
+                            <h3
+                                class="truncate w-full text-xl font-medium bg-gradient-to-r from-slate-900 via-blue-900 to-slate-800 bg-clip-text text-transparent mb-3 uppercase font-[Oswald] tracking-widest">
+                                <?= $titre ?>
+                            </h3>
+                            <?php
+                            // Get the WYSIWYG content
+                            $description = get_field('description', $collection->ID);
 
-                        // Strip HTML tags and decode entities
-                        $plain_text = wp_strip_all_tags($description);
+                            // Strip HTML tags and decode entities
+                            $plain_text = wp_strip_all_tags($description);
 
-                        // Truncate to 150 characters (or whatever you want)
-                        $max_length = 135;
-                        if (strlen($plain_text) > $max_length) {
-                            $truncated = mb_substr($plain_text, 0, $max_length) . '...';
-                        } else {
-                            $truncated = $plain_text;
-                        }
-                        ?>
-                        <p class="text-gray-600 mb-4 leading-relaxed font-[Inter]"><?= esc_html($truncated) ?></p>
-                        <div href="<?= get_permalink($collection->ID); ?>" class="w-full sm:w-auto text-white rounded-xl
+                            // Truncate to 150 characters (or whatever you want)
+                            $max_length = 135;
+                            if (strlen($plain_text) > $max_length) {
+                                $truncated = mb_substr($plain_text, 0, $max_length) . '...';
+                            } else {
+                                $truncated = $plain_text;
+                            }
+                            ?>
+                            <p class="text-gray-600 mb-4 leading-relaxed font-[Inter]"><?= esc_html($truncated) ?></p>
+                            <div href="<?= get_permalink($collection->ID); ?>" class="w-full sm:w-auto text-white rounded-xl
                                 first:rounded-t-lg last:rounded-b-lg py-4 sm:py-5 sm:px-8 tracking-wider sm:tracking-widest text-md
                                 text-center flex items-center justify-center gap-2 bg-gradient-to-r from-slate-900 via-blue-900 to-slate-800 
                             [background-size:200%_200%] [background-position:left_center] 
                             transition-[background-position] duration-500 ease-out
                             hover:[background-position:right_center] 
                             group-hover:[background-position:right_center]">
-                            EN SAVOIR PLUS
-                            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
-                                class="inline-block">
-                                <path d="M7 17L17 7M17 7H9M17 7V15" stroke="currentColor" stroke-width="2"
-                                    stroke-linecap="round" stroke-linejoin="round"></path>
-                            </svg>
+                                EN SAVOIR PLUS
+                                <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+                                    class="inline-block">
+                                    <path d="M7 17L17 7M17 7H9M17 7V15" stroke="currentColor" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round"></path>
+                                </svg>
+                            </div>
                         </div>
+                    </a>
+                <?php endforeach; ?>
+            </div>
+        <?php else: ?>
+            <div
+                class="container max-w-7xl mx-auto rounded-lg min-h-[500px] xl:h-96 flex items-center justify-center border bg-card shadow-sm bg-gradient-to-r from-slate-900 via-blue-900 to-slate-800 text-white">
+                <div class="p-8 text-center">
+                    <div class="mb-8 flex justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                            class="lucide lucide-briefcase w-12 h-12">
+                            <path d="M16 20V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path>
+                            <rect width="20" height="14" x="2" y="6" rx="2"></rect>
+                        </svg>
                     </div>
-                </a>
-            <?php endforeach; ?>
-        </div>
+                    <h2 class="text-2xl mb-4 uppercase font-[Oswald] tracking-widest">Aucun poste disponible actuellement
+                    </h2>
+                    <p class="text-white/80 mb-6 max-w-2xl mx-auto font-[Inter] text-lg sm:text-xl ">Nous n'avons pas
+                        d'offres d'emploi ouvertes pour le moment, mais nous recrutons régulièrement de nouveaux talents.
+                        Revenez consulter cette page prochainement ou postulez spontanément.</p>
+                    <div class="">
+                        <a href="mailto:hakitebio17@gmail.com?subject=Candidature%20Spontanée"
+                            class="w-full sm:w-auto sm:min-w-[320px] md:min-w-[400px] bg-white text-gray-900 rounded-xl hover:bg-gray-100 transition-colors duration-200 first:rounded-t-lg last:rounded-b-lg py-4 sm:py-5 sm:px-8 tracking-wider sm:tracking-widest text-md text-center uppercase flex items-center justify-center gap-2">
+                            Postuler spontanément
+                            <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor"
+                                xmlns="http://www.w3.org/2000/svg" class="inline-block">
+                                <path d="M7 17L17 7M17 7H9M17 7V15" stroke="currentColor" stroke-width="2"
+                                    stroke-linecap="round" stroke-linejoin="round">
+                                </path>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        <?php endif; ?>
     </div>
 </section>
 
