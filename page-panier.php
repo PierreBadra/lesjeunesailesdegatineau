dff31c154252ff3c14549814aed1989b719666b1
pierre-badra <PierreBadra@outlook.com>
2025-06-14 03:54:15 -0400

    Addded icon

--- page-panier.php
+++ page-panier.php
@@ -72,7 +72,21 @@ get_header();
                                                     ?>
                                                 </h3>
                                                 <div class="text-sm text-gray-600 mb-2" __v0_r="0,4571,4599">Du
-                                                    <span><?= $date_de_debut ?></span> au <span><?= $date_de_fin ?></span>
+                                                    <svg data-v-56bd7dfc="" xmlns="http://www.w3.org/2000/svg" width="24"
+                                                        height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                        class="lucide lucide-calendar-days-icon lucide-calendar-days">
+                                                        <path d="M8 2v4"></path>
+                                                        <path d="M16 2v4"></path>
+                                                        <rect width="18" height="18" x="3" y="4" rx="2"></rect>
+                                                        <path d="M3 10h18"></path>
+                                                        <path d="M8 14h.01"></path>
+                                                        <path d="M12 14h.01"></path>
+                                                        <path d="M16 14h.01"></path>
+                                                        <path d="M8 18h.01"></path>
+                                                        <path d="M12 18h.01"></path>
+                                                        <path d="M16 18h.01"></path>
+                                                    </svg> <span><?= $date_de_debut ?></span> au <span><?= $date_de_fin ?></span>
                                                 </div>
                                                 <div class="text-sm text-gray-600 mb-2">
                                                     <?php
