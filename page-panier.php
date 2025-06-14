48df9d598df864ba36cf4215309719f8a6c2b200
pierre-badra <PierreBadra@outlook.com>
2025-06-14 03:13:54 -0400

    More styling tweaks

--- page-panier.php
+++ page-panier.php
@@ -61,10 +61,10 @@ get_header();
 
                                             <!-- Product Details -->
                                             <div class="flex-grow">
-                                                <h3 class="font-bold bg-gradient-to-r from-slate-900 via-blue-900 to-slate-800 bg-clip-text text-transparent uppercase font-[Oswald] tracking-widest mb-1 truncate">
+                                                <h3 class="font-bold bg-gradient-to-r transition-colors duration-200 gradient-animate from-slate-900 via-blue-900 to-slate-800 bg-clip-text text-transparent uppercase font-[Oswald] tracking-widest mb-1 truncate">
                                                     <?php
                                                     if ($product_permalink) {
-                                                        echo '<a href="' . esc_url($product_permalink) . '" class="hover:text-blue-600 truncate">' . $_product->get_name() . '</a>';
+                                                        echo '<a href="' . esc_url($product_permalink) . '>' . $_product->get_name() . '</a>';
                                                     } else {
                                                         echo $_product->get_name();
                                                     }
