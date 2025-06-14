5f2a324f4eb6faf387dfa1e1be8c72a007d0151f
pierre-badra <PierreBadra@outlook.com>
2025-06-14 02:18:10 -0400

    Customized add to cart button to match theme

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -88,14 +88,14 @@ $product = wc_get_product();
 					}
 					?>
 
-					<button type="submit" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>"
-						class="w-full sm:w-auto bg-gradient-to-r from-slate-900 via-blue-900 to-slate-800 text-white rounded-xl py-4 sm:py-5 sm:px-8 tracking-wider sm:tracking-widest text-md text-center flex items-center justify-center gap-2 transition-colors duration-200 gradient-animate">
+					<button type="submit" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>" class="w-full sm:w-auto bg-gradient-to-r from-slate-900 via-blue-900 to-slate-800 text-white rounded-xl
+		  first:rounded-t-lg last:rounded-b-lg py-4 sm:py-5 sm:px-8 tracking-wider sm:tracking-widest text-md
+		  text-center flex items-center justify-center gap-2 transition-colors duration-200 gradient-animate">
 						AJOUTER AU PANIER
 						<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
 							class="inline-block">
-							<path
-								d="M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.7 15.3C4.3 15.7 4.6 16.5 5.1 16.5H17M17 13V17C17 18.1 16.1 19 15 19H9C7.9 19 7 18.1 7 17V13M17 13H7"
-								stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+							<path d="M7 17L17 7M17 7H9M17 7V15" stroke="currentColor" stroke-width="2"
+								stroke-linecap="round" stroke-linejoin="round" />
 						</svg>
 					</button>
 				</form>
