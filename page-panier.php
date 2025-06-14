0c107cfc928bb170643312828c095e433aeea0e4
pierre-badra <PierreBadra@outlook.com>
2025-06-14 03:57:53 -0400

    Added right widrth and height to icon

--- page-panier.php
+++ page-panier.php
@@ -71,11 +71,11 @@ get_header();
                                                     }
                                                     ?>
                                                 </h3>
-                                                <div class="text-sm text-gray-600 mb-2 flex items-center gap-2" __v0_r="0,4571,4599"><svg data-v-56bd7dfc=""
-                                                        xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                                        stroke-linecap="round" stroke-linejoin="round"
-                                                        class="lucide lucide-calendar-days-icon lucide-calendar-days">
+                                                <div class="text-sm text-gray-600 mb-2 flex items-center gap-2"
+                                                    __v0_r="0,4571,4599"><svg data-v-56bd7dfc="" xmlns="http://www.w3.org/2000/svg"
+                                                        width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                        class="w-4 h-4 lucide lucide-calendar-days-icon lucide-calendar-days">
                                                         <path d="M8 2v4"></path>
                                                         <path d="M16 2v4"></path>
                                                         <rect width="18" height="18" x="3" y="4" rx="2"></rect>
