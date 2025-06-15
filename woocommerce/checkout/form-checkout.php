2ce31843412e313048db847933012bc1d240ee5e
pierre-badra <PierreBadra@outlook.com>
2025-06-15 02:36:44 -0400

    test

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -33,16 +33,22 @@ if (!$checkout->is_registration_enabled() && $checkout->is_registration_required
 	<div class="container mx-auto max-w-7xl">
 		<div class="mb-8">
 			<a href="/panier"
-				class="inline-flex items-center gap-2 text-blue-950 hover:text-blue-900 transition-colors"><svg
-					xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+				class="inline-flex items-center gap-2 text-blue-950 hover:text-blue-900 transition-colors">
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
 					stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
 					class="lucide lucide-arrow-left w-4 h-4">
 					<path d="m12 19-7-7 7-7"></path>
 					<path d="M19 12H5"></path>
-				</svg>Retour au panier
+				</svg>
+				Retour au panier
 			</a>
 		</div>
-		<?php wc_print_notices(); ?>
+
+		<?php
+		// Output error messages and notices here, OUTSIDE the grid
+		do_action('woocommerce_before_checkout_form', $checkout);
+		?>
+
 		<form class="grid lg:grid-cols-2 gap-8 checkout woocommerce-checkout" name="checkout" method="post"
 			action="<?php echo esc_url(wc_get_checkout_url()); ?>" enctype="multipart/form-data"
 			aria-label="<?php echo esc_attr__('Checkout', 'woocommerce'); ?>">
@@ -59,7 +65,6 @@ if (!$checkout->is_registration_enabled() && $checkout->is_registration_required
 				<?php do_action('woocommerce_checkout_before_order_review'); ?>
 				<?php do_action('woocommerce_checkout_order_review'); ?>
 				<?php do_action('woocommerce_checkout_after_order_review'); ?>
-
 			</div>
 		</form>
 	</div>
