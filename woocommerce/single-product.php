2b01dd013bbcd3714d5479707377a0c4031e5f3d
pierre-badra <PierreBadra@outlook.com>
2025-06-16 01:29:46 -0400

    Fixed minor bug

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -15,7 +15,7 @@ if ($terms && !is_wp_error($terms)) {
 
 <section class="w-full p-6 pt-40">
 	<div class="mb-8 container max-w-7xl mx-auto">
-		<a href="<?= $category_url; ?>"
+		<a href="/<?= $category_url; ?>"
 			class="inline-flex items-center gap-2 text-blue-950 hover:text-blue-900 transition-colors">
 			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
 				stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
