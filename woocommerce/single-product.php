660a23afc124e34eb404e102513efdae35a716c6
pierre-badra <PierreBadra@outlook.com>
2025-06-16 02:52:25 -0400

    Remove commande page

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -85,10 +85,13 @@ if ($terms && !is_wp_error($terms)) {
 		  rounded-xl py-4 sm:py-5 sm:px-8 tracking-wider sm:tracking-widest text-md
 		  text-center flex items-center justify-center gap-2 transition-colors duration-200 gradient-animate">
 						AJOUTER AU PANIER
-						<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
-							class="inline-block">
-							<path d="M7 17L17 7M17 7H9M17 7V15" stroke="currentColor" stroke-width="2"
-								stroke-linecap="round" stroke-linejoin="round" />
+						<svg data-v-56bd7dfc="" xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+							viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+							stroke-linejoin="round" class="inline-block">
+							<circle cx="8" cy="21" r="1"></circle>
+							<circle cx="19" cy="21" r="1"></circle>
+							<path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12">
+							</path>
 						</svg>
 					</button>
 				</form>
