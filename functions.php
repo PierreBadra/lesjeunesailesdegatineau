3a0f55f105c29872aa8ea74a06b3a7135c5f6e0a
pierre-badra <PierreBadra@outlook.com>
2025-07-01 19:41:12 -0400

    Uncommented code that removes admin panel menu items

--- functions.php
+++ functions.php
@@ -54,10 +54,10 @@ add_shortcode('explorez', 'explorez_shortcode');
 
 // Actions
 add_action('admin_menu', function () {
-    remove_menu_page('edit.php');
-    remove_menu_page('edit-comments.php');
-    remove_menu_page('tools.php');
-    remove_menu_page('edit.php?post_type=collection');
+    // remove_menu_page('edit.php');
+    // remove_menu_page('edit-comments.php');
+    // remove_menu_page('tools.php');
+    // remove_menu_page('edit.php?post_type=collection');
 });
 
 
